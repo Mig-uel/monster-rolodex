6e027c5eb33687b48fd23572c1cfc2a32c4e142e
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-04-20 00:02:18 -0400

    added component folders and files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-
 import './App.css';
 
 class App extends Component {
@@ -7,14 +6,58 @@ class App extends Component {
   constructor() {
     super();
 
+    // STATE
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: ''
+    };
+    console.log('constructor')
+  }
+
+  // API FETCH
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return { monsters: users }
+      }, () => console.log(this.state)))
+  }
+
+  onSearch = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField }
+    })
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearch } = this;
+
+    console.log('render')
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+
     return (
       <div className="App" >
+        <input type="search"
+          className="search-box"
+          placeholder="search monsters..."
+          onChange={onSearch}
+        />
+        {
+          filteredMonsters.map(monster => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
