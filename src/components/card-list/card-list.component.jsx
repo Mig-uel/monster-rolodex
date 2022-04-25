68a0eac783dcf7bda991bb02bc6c5defcd0ed228
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-04-25 12:41:48 -0400

    card component added.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import Card from '../card/card.component'
+
 import './card-list.styles.css';
 import './card.styles.css';
 
@@ -11,14 +13,7 @@ class CardList extends Component {
     return (
       <div className='card-list'>
         {
-          monsters.map(monster => {
-            const { id, name, email } = monster
-            return <div className="card-container" key={id}>
-              <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={`monster ${name}`} />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          })
+          monsters.map(monster => <Card monster={monster} />)
         }
       </div>
     )
