a66df764e1d2973418bee299accb8b46fde6f512
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-04-28 12:22:58 -0400

    swithced to functional components.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react'
 
 import './App.css';
 import CardList from './components/card-list/card-list.component'
@@ -6,6 +6,31 @@ import SearchBox from './components/search-box/search-box.component'
 
 
 const App = () => {
+
+  const [searchField, setSeachField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setMonsters(users))
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearch = (e) => {
+    const searchFieldString = e.target.value.toLocaleLowerCase();
+
+    setSeachField(searchFieldString);
+  }
+
   return (
     <div className="App" >
       <h1 className="app-title">Monster Rolodex</h1>
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,26 +1,31 @@
-import { Component } from 'react';
-
 import './search-box.styles.css'
 
-class SearchBox extends Component {
+const SearchBox = ({ onChangeHandler, placeholder, className }) => {
 
-  constructor() {
-    super()
-
-    this.state = {};
-  }
-
-  render() {
-    console.log(this)
-    const { onChangeHandler, placeholder, className } = this.props;
-    return (
-      <input type="search"
-        className={`search-box ${className}`}
-        placeholder={placeholder}
-        onChange={onChangeHandler}
-      />
-    )
-  }
+  return (
+    <input type="search"
+      className={`search-box ${className}`}
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  );
 }
 
 export default SearchBox;
+
+// class SearchBox extends Component {
+
+//   render() {
+//     console.log(this)
+//     const { onChangeHandler, placeholder, className } = this.props;
+//     return (
+//       <input type="search"
+//         className={`search-box ${className}`}
+//         placeholder={placeholder}
+//         onChange={onChangeHandler}
+//       />
+//     )
+//   }
+// }
+
+// export default SearchBox;
